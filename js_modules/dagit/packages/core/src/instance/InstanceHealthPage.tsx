7769c94625369b3daa4326c93f0e2e5a919ec882
Johann Miller <johann@elementl.com>
2021-11-15 17:42:38 -0500

    Remove unused workspace gql query

--- js_modules/dagit/packages/core/src/instance/InstanceHealthPage.tsx
+++ js_modules/dagit/packages/core/src/instance/InstanceHealthPage.tsx
@@ -6,7 +6,6 @@ import {Box} from '../ui/Box';
 import {ColorsWIP} from '../ui/Colors';
 import {PageHeader} from '../ui/PageHeader';
 import {Heading, Subheading} from '../ui/Text';
-import {REPOSITORY_LOCATIONS_FRAGMENT} from '../workspace/WorkspaceContext';
 
 import {DaemonList} from './DaemonList';
 import {INSTANCE_HEALTH_FRAGMENT} from './InstanceHealthFragment';
@@ -48,11 +47,7 @@ const INSTANCE_HEALTH_QUERY = gql`
     instance {
       ...InstanceHealthFragment
     }
-    workspaceOrError {
-      ...RepositoryLocationsFragment
-    }
   }
 
   ${INSTANCE_HEALTH_FRAGMENT}
-  ${REPOSITORY_LOCATIONS_FRAGMENT}
 `;
